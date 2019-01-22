89845b1e6ead48e3740eb9c2bb5d669eddf56374
garethosullivan <gareth.osullivan@icloud.com>
2019-01-22 20:35:59 +0000

    Added buildUrl withQueryItems func

--- OnTheMap/Model/UdacityConvenience.swift
+++ OnTheMap/Model/UdacityConvenience.swift
@@ -21,4 +21,20 @@ extension UdacityClient {
         return components.url!
     }
     
+    //MARK: Build Url with Query Items
+    func buildUrl( _ host: String, _ path: String, withQueryItems: [String:AnyObject]) -> URL {
+        //Function to build url with query items added
+        var components = URLComponents()
+        components.scheme = UrlComponents.ApiScheme
+        components.host = host
+        components.path = path
+        components.queryItems = [URLQueryItem]()
+        //For loop to add key and value from query items array to queryItems
+        for (key, value) in withQueryItems {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        return components.url!
+    }
+    
 }
