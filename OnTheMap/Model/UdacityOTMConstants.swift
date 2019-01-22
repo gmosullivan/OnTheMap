d28b24337cb6f9a71d9b75d2195a0281def96eb8
garethosullivan <gareth.osullivan@icloud.com>
2019-01-22 18:55:06 +0000

    Added HTTPBody struct

--- OnTheMap/Model/UdacityOTMConstants.swift
+++ OnTheMap/Model/UdacityOTMConstants.swift
@@ -8,4 +8,17 @@
 
 extension UdacityClient {
     
+    struct HTTPBodyKeys {
+        static let firstName = "firstName"
+        static let lastName = "lastName"
+        static let latitude = "latitude"
+        static let longitude = "longitude"
+        static let mapString = "mapString"
+        static let mediaUrl = "mediaURL"
+        static let password = "password"
+        static let udacity = "udacity"
+        static let uniqueKey = "uniqueKey"
+        static let username = "username"
+    }
+    
 }
