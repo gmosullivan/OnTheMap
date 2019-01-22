a584b1d7c5272dbf64bc68425ed9dca01ea5191f
garethosullivan <gareth.osullivan@icloud.com>
2019-01-22 18:50:30 +0000

    Added UdacityOTMConstants extension to store constants

---
+++ OnTheMap/Model/UdacityOTMConstants.swift
@@ -0,0 +1,11 @@
+//
+//  UdacityOTMConstants.swift
+//  OnTheMap
+//
+//  Created by Gareth O'Sullivan on 22/01/2019.
+//  Copyright © 2019 Locust Redemption. All rights reserved.
+//
+
+extension UdacityClient {
+    
+}
