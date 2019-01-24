9a6afabbea96828d32355f825090406c58098033
garethosullivan <gareth.osullivan@icloud.com>
2019-01-24 14:15:55 +0000

    Added OTMMapViewController and OTMTableViewController classes

---
+++ OnTheMap/Controller/OTMMapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OTMMapViewController.swift
+//  OnTheMap
+//
+//  Created by Gareth O'Sullivan on 24/01/2019.
+//  Copyright © 2019 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class OTMMapViewController: UIViewController, MKMapViewDelegate {
+
+    //MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ OnTheMap/Controller/OTMTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  OTMTableViewController.swift
+//  OnTheMap
+//
+//  Created by Gareth O'Sullivan on 24/01/2019.
+//  Copyright © 2019 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+
+class OTMTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+}
