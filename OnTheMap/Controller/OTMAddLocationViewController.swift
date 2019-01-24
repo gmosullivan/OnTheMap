f4265811cfe2c6db98494ff54f8f3fe55b2e82a9
garethosullivan <gareth.osullivan@icloud.com>
2019-01-24 16:27:33 +0000

    Added OTMAddLocationViewController class

---
+++ OnTheMap/Controller/OTMAddLocationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OTMAddLocationViewController.swift
+//  OnTheMap
+//
+//  Created by Gareth O'Sullivan on 24/01/2019.
+//  Copyright © 2019 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class OTMAddLocationViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
